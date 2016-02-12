903a38ae17af9ff71170c6d31efccb0008a14d68
zskyfly <zac@lyft.com>
2016-02-12 15:59:27 -0800

    Milestone 3 completed

---
+++ instagram/PhotosTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  PhotosTableViewCell.swift
+//  instagram
+//
+//  Created by Zachary Matthews on 2/12/16.
+//  Copyright © 2016 zskyfly productions. All rights reserved.
+//
+
+import AFNetworking
+import UIKit
+
+class PhotosTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- instagram/PhotosViewController.swift
+++ instagram/PhotosViewController.swift
@@ -8,10 +8,16 @@
 
 import UIKit
 
-class PhotosViewController: UIViewController {
+class PhotosViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var data = NSArray()
+    @IBOutlet weak var photosTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        photosTableView.dataSource = self
+        photosTableView.delegate = self
+        photosTableView.rowHeight = 320
 
         let clientId = "e05c462ebd86446ea48a5af73769b602"
         let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
@@ -27,7 +33,10 @@ class PhotosViewController: UIViewController {
                 if let data = dataOrNil {
                     if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
                         data, options:[]) as? NSDictionary {
-                            NSLog("response: \(responseDictionary)")
+//                            NSLog("response: \(responseDictionary)")
+                            self.data = responseDictionary["data"] as! NSArray
+                            NSLog("results returned: \(self.data.count)")
+                            self.photosTableView.reloadData()
                     }
                 }
         });
@@ -37,6 +46,28 @@ class PhotosViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("com.zskyfly.PhotosTableViewCell", forIndexPath: indexPath) as! PhotosTableViewCell
+        let index = indexPath.row
+        let data = self.data[index] as! NSDictionary
+        let lowResUrl = getLowResUrlFromData(data)
+        cell.photoImageView?.setImageWithURL(lowResUrl)
+        
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print ("photo count \(self.data.count)")
+        return self.data.count
+    }
+    
+    func getLowResUrlFromData(data: NSDictionary) -> NSURL {
+        let url = data.valueForKeyPath("images.low_resolution.url") as! String
+        return NSURL(string: url)!
     }
 
 
